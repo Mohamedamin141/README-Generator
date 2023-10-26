9025bd4c69ab16cc62a3d927131545641d4ee847
Mohamed Amin A Osman <mo5250760@gmail.com>
2023-10-26 17:13:03 +1100

    First commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,83 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type:"input",
+     message:"Your-Project-Title",
+     name:"title"
+  },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  {
+    type:"input",
+     message:"enter a description,",
+     name:"description"
+  },
 
-// TODO: Create a function to initialize app
+
+  {
+    type:"input",
+     message:"What are the steps required to install your project?",
+     name:"Installation"
+  },
+
+  {
+    type:"input",
+     message:"Provide instructions and examples for use?",
+     name:"Usage"
+  },
+
+  {
+    type:"input",
+     message:"List your collaborators, if any,",
+     name:"Credits"
+  },
+  {
+    type:"list",
+     message:"which License?",
+     name:"license",
+     choices:["MIT-License", "Boost-License", "Unlicense-license", "Apache-License", "GNU-GPLv3", "GNU-LGPLv3", "GNU-AGPLv3"]
+  },
+  {
+    type:"input",
+     message:"If your project has a lot of features, list them here.",
+     name:"Features"
+  },
+  {
+    type:"input",
+     message:"write tests instructions for your application",
+     name:"Tests"
+  },
+  {
+    type:"input",
+     message:"enter your GitHub username",
+     name:"username"
+  },
+  {
+    type:"input",
+     message:"enter your Email",
+     name:"email"
+  },
+ ];
+
+function writeToFile(fileName, data) {
+   fs.writeFile(fileName, data, err => err?
+    console.log(err): console.log("Success!")
+   )
+
+}
+
 function init() {}
-
+inquirer.prompt(questions)
+.then(function(data){
+  writeToFile("../README.md",generateMarkdown(data));
+  
+})
 // Function call to initialize app
 init();
+
+
+
+
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,62 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseBadge(license) {
+  if (license !== ""){
+    return `[![License](https://img.shields.io/badge/${license}-blue.svg)](https://opensource.org/licenses/${license})`
+  }
+  else {
+    return ""
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [license](#license)  
+- [Questions](#questions)
+- [Tests](#tests)
+  ## Description
+  
+  ${data.description}
+  
+  
+  ## Installation
+  
+  ${data.Installation}
+  
+  ## Usage
+  
+  ${data.Usage}
+  
+
+  
+  ## Credits
+  
+  ${data.Credits}
+  
+  ## License
+  
+  This application is covered under the ${renderLicenseBadge(data.license)} license.
+
+
+  ## Features
+  
+  ${data.Features}
+  
+  ## Tests
+  ${data.Tests}
+  
+  ## Questions
+  ### Contact
+  For additional questions, please reach out to me at ${data.email} <br/>
+  Check out my GitHub profile here: https://github.com/${data.username}
 `;
 }
-
 module.exports = generateMarkdown;
